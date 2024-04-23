432510d967cfd8c52c1f2b477e79c7c1b8988cf0
aliqyan-21 <aliqyanabid21@gmail.com>
2024-04-23 19:25:38 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var body = document.getElementById("body");
-  var isFirstVisit = localStorage.getItem("isFirstVisit");
+// Check if it's the initial visit or if the website was quit and revisited
+if (!localStorage.getItem("visited")) {
+  // If it's the initial visit, play the animations
+  playAnimations();
+  // Set the flag to indicate that the website has been visited
+  localStorage.setItem("visited", true);
+}
 
-  if (isFirstVisit === null) {
-    // First visit
-    localStorage.setItem("isFirstVisit", "false");
-  } else {
-    // Return visit
-    body.classList.remove("initial-visit");
-  }
-});
+// Function to play the animations
+function playAnimations() {
+  // Your animation code here
+  var element = document.querySelector(".sp-content");
+  var animationClasses = [
+    "frame-1",
+    "frame-2",
+    "frame-3",
+    "frame-4",
+    "frame-5",
+    "frame-6",
+  ];
+  animationClasses.forEach(function (className, index) {
+    setTimeout(function () {
+      element.classList.add(className);
+    }, index * 3000);
+  });
+}
